a41550147ee4b6165ef68f3da9890014cbdf1cee
sharon <nie8605@qq.com>
2014-07-30 11:43:41 +0800

    创建工程

---
+++ CJLApp/src/com/zw/cjl/Welcome.java
@@ -0,0 +1,13 @@
+package com.zw.cjl;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Welcome extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+}
